b84622740eb869df69b52e5ee7b58326b9f8c3ed
Jason Long <jason.long77@hotmail.com>
2020-09-05 03:03:34 +0000

    Conditional Sums solution

---
+++ conditional-sums.js
@@ -0,0 +1,23 @@
+const conditionalSum = function(values, condition) {
+  let sum = 0;
+  if (condition === "even"){
+    for (let i = 0; i < values.length; i++){
+      if (values[i] % 2 === 0){
+        sum += values[i];
+      }
+    }
+    return sum;
+  } else if (condition === "odd"){
+    for (let j = 0; j < values.length; j++){
+      if (values[j] % 2 === 1){
+        sum += values[j];
+      }
+    }
+    return sum;
+  }
+};
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
